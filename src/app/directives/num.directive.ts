a73f614048bd5d3de47643d89c92bc41a03adb8b
Mahsa <arabzadehmah@gmail.com>
2025-06-06 21:27:58 +0430

    feat(num.directive): implement numeric input validation directive

--- src/app/directives/num.directive.ts
+++ src/app/directives/num.directive.ts
@@ -4,5 +4,15 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appNum]',
 })
 export class NumDirective {
-  constructor() {}
+  constructor(private element: ElementRef<HTMLInputElement>) {}
+
+  @HostListener('input', ['$event']) onChange() {
+    let value = this.element.nativeElement;
+    const elValue = this.element.nativeElement.value;
+    const filterNumber = elValue.replace(/[^0-9]/g, '');
+
+    if (filterNumber !== elValue) {
+      value.value = filterNumber;
+    }
+  }
 }
