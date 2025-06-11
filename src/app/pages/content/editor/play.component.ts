053f05161b4881de9049b3198b537ed64c10368b
Mahsa <arabzadehmah@gmail.com>
2025-06-11 16:57:24 +0430

    feat(play.component): enhance CKEditor integration and improve component structure

--- src/app/pages/content/editor/play.component.ts
+++ src/app/pages/content/editor/play.component.ts
@@ -1,47 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, type OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import {
-  CKEditorModule,
   loadCKEditorCloud,
-  CKEditorCloudResult,
+  CKEditorModule,
+  type CKEditorCloudResult,
+  type CKEditorCloudConfig,
 } from '@ckeditor/ckeditor5-angular';
-import type {
-  ClassicEditor,
-  EditorConfig,
-} from 'https://cdn.ckeditor.com/typings/ckeditor5.d.ts';
+import type { ClassicEditor } from 'https://cdn.ckeditor.com/typings/ckeditor5.d.ts';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+
+import { LICENSE_KEY } from '../../../../const/constants';
+
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+const cloudConfig = {
+  version: '45.2.0',
+} satisfies CKEditorCloudConfig;
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CKEditorModule,
+    CardModule,
+    FormsModule,
+    ButtonModule,
+  ],
   templateUrl: './play.component.html',
   styleUrls: ['./play.component.css'],
-  imports: [CommonModule, CKEditorModule],
-  standalone: true,
+  encapsulation: ViewEncapsulation.None,
 })
-export class PlayComponent {
-  Editor: typeof ClassicEditor | null = null;
+export class PlayComponent implements OnInit {
+  public Editor: typeof ClassicEditor | null = null;
+  public config: EditorConfig | null = null;
+  items: string = '';
+  savedNote: string[] = [];
 
-  config: EditorConfig | null = null;
+  public ngOnInit(): void {
+    loadCKEditorCloud(cloudConfig).then(this._setupEditor.bind(this));
 
-  ngOnInit(): void {
-    loadCKEditorCloud({
-      version: '45.1.0',
-      premium: true,
-    }).then(this._setupEditor.bind(this));
+    const storedNote = localStorage.getItem('note');
+    if (storedNote) {
+      this.savedNote = JSON.parse(storedNote);
+    }
   }
 
-  private _setupEditor(
-    cloud: CKEditorCloudResult<{ version: '45.1.0'; premium: true }>
-  ) {
-    const { ClassicEditor, Essentials, Paragraph, Bold, Italic } =
-      cloud.CKEditor;
-    const { FormatPainter } = cloud.CKEditorPremiumFeatures;
+  convertText(text: string) {
+    const element = document.createElement('div');
+    element.innerHTML = text;
+    return element.innerText;
+  }
+
+  handleSaveNote(event: Event) {
+    event.preventDefault();
+    if (this.items) {
+      this.savedNote.push(this.items);
+      this.items = '';
+
+      localStorage.setItem('note', JSON.stringify(this.savedNote));
+    }
+  }
+
+  private _setupEditor(cloud: CKEditorCloudResult<typeof cloudConfig>) {
+    const {
+      ClassicEditor,
+      AutoLink,
+      Autosave,
+      Bold,
+      Essentials,
+      Italic,
+      Paragraph,
+    } = cloud.CKEditor;
 
     this.Editor = ClassicEditor;
     this.config = {
-      licenseKey:
-        'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3Nzk0OTQzOTksImp0aSI6ImI3YjE4NjU4LWQ0NGMtNDI2MC1hMmU4LTgyN2ZlMjE2ZmE2ZCIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCIsIkUyUCIsIkUyVyJdLCJ2YyI6ImJlYmU5Njg3In0.4ONYuOFrs1hiZfdhdZnzcf2w1FbftjcHkyDiSyeVwk76JkO5ZSr_qq5uXTMU_hBwhTc2BrIOAoWWe3zZYlKJ_g',
-      plugins: [Essentials, Paragraph, Bold, Italic, FormatPainter],
-      toolbar: ['undo', 'redo', '|', 'bold', 'italic', '|', 'formatPainter'],
+      toolbar: {
+        items: ['undo', 'redo', '|', 'bold', 'italic'],
+        shouldNotGroupWhenFull: false,
+      },
+      plugins: [AutoLink, Autosave, Bold, Essentials, Italic, Paragraph],
+      licenseKey: LICENSE_KEY,
+
+      placeholder: 'Type or paste your notes here!',
     };
   }
 }
